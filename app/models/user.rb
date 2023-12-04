42331453003867d874c136bf981bebb38ab3dfbc
Munawar Hussain <munawarhussa.7@gmail.com>
2023-12-04 14:48:52 +0500

    Call back practice

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   validates :email, presence:true
   after_create :log_new_user
+  before_validation do
+    puts "User validation started"
+  end
 
+  after_validation -> {puts "User validation completed"}
+  
   private
     def log_new_user
       print "New user created"
-      p "user is #{self.name}"
     end
 end
