b07589d6efd14c47dfd52b5eb85380309fd702c8
Munawar Hussain <munawarhussa.7@gmail.com>
2023-12-06 16:27:30 +0500

    views with render

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def show 
+    render inline: "<p style='font-size: 30px; color: black;'>Hey! You made a request to the show method.</p>"
+  end
+
+  def edit
+    # render plain: "You can not edit now. Kindly be careful while creation"
+    # render html: helpers.tag.strong('Not Found')
+    # render js: "alert('Hello Rails');"
+    render body: "raw"
+
+
+  end
 end
